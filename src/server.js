0a2231f448091930a1f2a1d820f3799c898d5aaa
ERIKSONLOURES <erikson992@gmail.com>
2025-01-01 22:58:32 -0300

    method edt

--- src/server.js
+++ src/server.js
@@ -6,11 +6,13 @@
 //   - Método HTTP
 //   - URL
 // GET, POST, PUT, PATCH, DELETE
+
 // GET => Buscar um recurso do back-end
 // POST => Criar um recurso no back-end
 // PUT => Atualizar um recurso no back-end
 // PATCH => Atualizar uma informação específica de um recurso no back-end
 // DELETE => Deletar um recurso do back-end
+
 // GET /users => Buscando usuários no banc-end
 // POST /users => Criar um usuário no back-end
 
